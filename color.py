4cc305988f8c17cc58532af7317b0bbe13f2afb3
yun_j_sang(윤재상) <apple01644@gmail.com>
2019-08-27 20:34:56 +0900

    Add files via upload

---
+++ color.py
@@ -0,0 +1,43 @@
+import pygame, random
+pygame.init()
+surf = pygame.display.set_mode((400,400))
+
+
+names = []
+
+f_nam = open('names.dat', 'r')
+s = f_nam.read().split('\n')
+for _ in s:
+    names.append(_.split(',')[0])
+
+f_nam.close()
+
+data = open('colors.dat', 'a')
+name = open('names.dat', 'w')
+
+
+
+for _ in range(100):
+    pygame.event.get()
+    r = random.randint(0,255)
+    g = random.randint(0,255)
+    b = random.randint(0,255)
+    surf.fill((r,g,b))
+    pygame.display.flip()
+    s = input('%d, %d, %d>' % (r,g,b))
+    i = 0
+    for _ in names:
+        if _ == s:
+            s = None
+            break
+        i += 1
+    if s != None:
+        print('append', s)
+        names.append(s)
+        name.write('%s, %d\n' % (s, i))
+    data.write('%d,%d,%d:%d\n'%(r, g, b, i))
+        
+
+pygame.quit()
+data.close()
+name.close()
